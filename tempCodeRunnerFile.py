6bc3e63029d2e53cfdad1e67f4863c8a91b0019a
Prathik R <142076388+PrathikRA@users.noreply.github.com>
2024-09-26 15:33:02 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+def add():
+#     CSV.initilaize_csv()
+#     date = get_date(
+#         "enter the date of transations (dd-mm-yyyy)", allow_default=True)
+#     amount = get_amount()
+#     descr = get_descrip()
+#     cate = get_cate()
+#     CSV.add_entry(date, amount, cate, descr)
